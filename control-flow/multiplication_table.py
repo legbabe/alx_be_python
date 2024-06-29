1769d33ef3a0c8b012becdb468b50794cda491dd
Lotsu Daniel Ajayi <ads21b00261y@ait.edu.gh>
2024-06-29 14:41:42 +0000

    updated the print statement

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,6 +1,4 @@
 number = int(input("Enter a number to see its multiplication table: "))
 
 for value in range(1,11):
-    final = value * number
-    print(number, '*', value, '= ', final)
-    print('yes')
+    print(f"{number} * {value} = {number * value}")
